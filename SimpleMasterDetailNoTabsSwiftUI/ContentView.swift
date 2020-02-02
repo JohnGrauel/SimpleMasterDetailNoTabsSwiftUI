4807970f4bcf7a9af42f94c4b264c949be183681
John Grauel <john@carbonrose.com>
2020-02-01 17:29:29 -0800

    Initial Commit

---
+++ SimpleMasterDetailNoTabsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleMasterDetailNoTabsSwiftUI
+//
+//  Created by John Grauel on 2/1/20.
+//  Copyright © 2020 John Grauel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
